97df7ef8acf742fa12717550d42f99219365e6b1
Justin <justinrkinsman@gmail.com>
2022-05-30 12:26:41 -0300

    Create card system for library

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 let myLibrary = [];
 
 function Book() {
-    this.title = document.getElementById('title').value,
+    this.title = document.getElementById('title').value
     this.author = document.getElementById('author').value
     this.page = document.getElementById('page').value
     this.read = document.querySelector('input[name="read"]:checked').value
-    this.info = `${this.title} by ${this.author}, ${this.page} pages. have you read this book? ${this.read}`
+    this.info = `${this.title} by ${this.author}, ${this.page} pages. Have you read this book? ${this.read}`
 }
 
 let btn = document.getElementById('add')
@@ -15,11 +15,26 @@ function addBookToLibrary(){
     newBook = new Book
     const para = document.createElement('p');
     const node = document.createTextNode(newBook.info)
+    const del = document.createElement("button")
+    del.innerText = 'Delete'
+    const change = document.createElement('button')
+    change.innerText = 'Change Read Status'
     para.appendChild(node)
-    const element = document.getElementById('card')
-    element.appendChild(para)
+    const element = document.getElementById('table')
+    const card = document.createElement('div')
+    element.appendChild(card)
+    card.appendChild(para)
+    card.appendChild(del)
+    card.appendChild(change)
+    card.setAttribute('style', 'background-color: salmon; color: white;')
     console.log(newBook.info)
 }
+
+
+
+
+
+
 /*
 function addBookToLibrary(){
     let titleForm = document.getElementById('title').value
