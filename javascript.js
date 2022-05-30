7d9a3f3e2637e37554303d5623338009a01da00d
Justin <justinrkinsman@gmail.com>
2022-05-30 16:02:17 -0300

    Create function to clear screen

--- javascript.js
+++ javascript.js
@@ -5,31 +5,48 @@ function Book() {
     this.author = document.getElementById('author').value
     this.page = document.getElementById('page').value
     this.read = document.querySelector('input[name="read"]:checked').value
-    this.info = `${this.title} by ${this.author}, ${this.page} pages. Have you read this book? ${this.read}`
+    this.info = (`${this.title} by ${this.author} \ ${this.page} pages\ Read? ${this.read}`)
 }
 
 let btn = document.getElementById('add')
-    btn.addEventListener('click', addBookToLibrary);
+    btn.addEventListener('click', createTable);
 
 function addBookToLibrary(){
     newBook = new Book
-    const para = document.createElement('p');
-    const node = document.createTextNode(newBook.info)
-    const del = document.createElement("button")
-    del.innerText = 'Delete'
-    const change = document.createElement('button')
-    change.innerText = 'Change Read Status'
-    para.appendChild(node)
-    const element = document.getElementById('table')
-    element.style.display = 'grid'
-    const card = document.createElement('div')
-    element.appendChild(card)
-    card.appendChild(para)
-    card.appendChild(del)
-    card.appendChild(change)
-    card.setAttribute('style', 'background-color: #000080; color: white;')
-    console.log(newBook.info)
+    myLibrary.push(newBook)
+    return(myLibrary)
 }
+
+function clearScreen() {
+    let element = document.getElementById('table') 
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+function createTable() {
+    clearScreen()
+    addBookToLibrary()
+    myLibrary.forEach(function(number) {
+        const para = document.createElement('p');
+        const node = document.createTextNode(number)
+        const del = document.createElement("button")
+        del.innerText = 'Delete'
+        const change = document.createElement('button')
+        change.innerText = 'Change Read Status'
+        para.appendChild(node)
+        const element = document.getElementById('table')
+        element.style.display = 'grid'
+        const card = document.createElement('div')
+        element.appendChild(card)
+        card.appendChild(para)
+        card.appendChild(del)
+        card.appendChild(change)
+        card.setAttribute('style', 'background-color: #000080; color: white;')
+        console.log(myLibrary.join(' '))  
+    })
+}
+
 
 
 
@@ -37,14 +54,5 @@ function addBookToLibrary(){
 
 
 /*
-function addBookToLibrary(){
-    let titleForm = document.getElementById('title').value
-    let authorForm = document.getElementById('author').value
-    let pageForm = document.getElementById('page').value
-    let bookForm = new Book(titleForm, authorForm, pageForm)
-    console.log(bookForm)
-    
-    /*
     myLibrary.splice(0, 4, title, author, page, read)
     return myLibrary.join(' ')*/
-//}
