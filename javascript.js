26e70cfb3f642c40cf3cf61909fb8da671400c7c
Justin <justinrkinsman@gmail.com>
2022-05-31 13:34:48 -0300

    Crate table for displaying books

--- javascript.js
+++ javascript.js
@@ -5,16 +5,40 @@ function Book() {
     this.author = document.getElementById('author').value
     this.page = document.getElementById('page').value
     this.read = document.querySelector('input[name="read"]:checked').value
-    this.info = (`${this.title} by ${this.author} \ ${this.page} pages\ Read? ${this.read}`)
 }
 
 let btn = document.getElementById('add')
-    btn.addEventListener('click', createTable);
+    btn.addEventListener('click', addRow);
 
 function addBookToLibrary(){
     newBook = new Book
     myLibrary.push(newBook)
-    return(myLibrary)
+    return myLibrary
+}
+
+function clearForm() {
+    document.getElementById('title').value = ''
+    document.getElementById('author').value = ''
+    document.getElementById('page').value = ''
+}
+
+function addRow() {
+    addBookToLibrary()
+    clearForm()
+    let table = document.getElementById('table')
+    let row = table.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1)
+    let cell3 = row.insertCell(2)
+    let cell4 = row.insertCell(3)
+    let cell5 = row.insertCell(4)
+    let cell6 = row.insertCell(5)
+    cell1.innerText = `${newBook.title}`;
+    cell2.innerText = `${newBook.author}`
+    cell3.innerText = `${newBook.page}`
+    cell4.innerText = `${newBook.read}`
+    cell5.innerText = 'Delete'
+    cell6.innerText = 'Change Read Status'
 }
 
 function clearScreen() {
@@ -29,12 +53,14 @@ function createTable() {
     addBookToLibrary()
     myLibrary.forEach(function(number) {
         const para = document.createElement('p');
-        const node = document.createTextNode(number)
+        const title = document.createTextNode(myLibrary[title])
         const del = document.createElement("button")
         del.innerText = 'Delete'
+        del.setAttribute('id', 'delBtn')
         const change = document.createElement('button')
         change.innerText = 'Change Read Status'
-        para.appendChild(node)
+        change.setAttribute('id', 'readStatus')
+        para.appendChild(title)
         const element = document.getElementById('table')
         element.style.display = 'grid'
         const card = document.createElement('div')
@@ -47,7 +73,8 @@ function createTable() {
     })
 }
 
-
+/*let delBtn = document.getElementById('delBtn')
+delBtn.addEventListener('click', removeCard())
 
 
 
